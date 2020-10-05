2d78623296b23e5b7e88e55ecb2cbbffc0085faa
javier mejia <proyectb2.1@gmail.com>
2020-10-05 14:15:13 -0600

    test

---
+++ src/main/java/com/niu2020/DemoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.niu2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCloudApplication.class, args);
+	}
+
+}
